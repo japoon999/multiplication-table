bc515245ed6570a00a08b66ef774c9c7fcf9c389
japoon999 <aziz.azizli.95@inbox.ru>
2023-11-01 17:36:43 +0400

    multiplication table programing

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let number = prompt("Ededi daxil et!")
+
+for (let index = 1; index <= 10; index++) {
+
+    document.writeln(`${number}*${index}=${number * index}<br>`)
+
+    // document.write(number + "x" + index + "=" + number * index + "<br>");
+
+    // console.log(number + "x" + index + "=" + number * index);
+
+} 
